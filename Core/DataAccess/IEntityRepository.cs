907b93a72d5a222315022fe660f77b8a0531467e
buse bahşi <buse-96@hotmail.com>
2021-02-07 00:51:54 +0300

    Proje dosyası ekle.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //generic constraint 
+    //IEntity:IEntity olabilir veya IEntity implemente eden bir nesne olabiir
+    //new() : newlenebilir olmalı
+   public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T,bool>> filter=null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Product Get(Expression<Func<Product, bool>> filter = null);
+    }
+}
